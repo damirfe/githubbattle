d5ef9de1ccbf3f1fc6bbc38da34d7dffd744ba39
Damir <damir.edu@gmail.com>
2018-10-02 02:33:49 +0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+var App = require('./components/App');
+
+// *state
+// *lifecycle events
+// *UI only one requierd 
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
