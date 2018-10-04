d3ea260a8275f5d385ac6b527408efac919d191a
Damir <damir.edu@gmail.com>
2018-10-04 23:44:14 +0200

    adding axios methods

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,8 +1,10 @@
 var React = require('react');
+var queryString = require('query-string');
 
 class Results extends React.Component {
     render() {
-        console.log(this.props)
+        var players = queryString.parse(this.props.location.search);
+        console.log(players);
         return (
             <div>Results</div>
         )
